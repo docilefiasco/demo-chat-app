e2b3ed9e761d7aae5850e898eedb286509c72bb8
docilefiasco <epshita.hs@gmail.com>
2021-06-20 19:07:32 +0530

    xml and class files added

--- app/src/main/java/com/example/chatapp/Model/Chatlist.java
+++ app/src/main/java/com/example/chatapp/Model/Chatlist.java
@@ -1,4 +1,21 @@
-package com.example.chatapp.Model;
+package com.example.demochatapp.Model;
 
 public class Chatlist {
+    private String id;
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public Chatlist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
+
